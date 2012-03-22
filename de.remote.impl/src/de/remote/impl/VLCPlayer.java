9d753d44eb722a79c811a5d2b6d4f56a46f978be
sebastian <pichlmeier.sebastian@googlemai.com>
2012-03-22 16:36:22 +0100

    add vcl remote control, todo: implementation

---
+++ de.remote.impl/src/de/remote/impl/VLCPlayer.java
@@ -0,0 +1,109 @@
+package de.remote.impl;
+
+import de.newsystem.rmi.protokol.RemoteException;
+import de.remote.api.IPlayer;
+import de.remote.api.IPlayerListener;
+import de.remote.api.PlayerException;
+
+public class VLCPlayer implements IPlayer{
+
+	@Override
+	public void play(String file) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void playPause() throws RemoteException, PlayerException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void quit() throws RemoteException, PlayerException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void next() throws RemoteException, PlayerException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void previous() throws RemoteException, PlayerException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void seekForwards() throws RemoteException, PlayerException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void seekBackwards() throws RemoteException, PlayerException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void volUp() throws RemoteException, PlayerException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void volDown() throws RemoteException, PlayerException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fullScreen() throws RemoteException, PlayerException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nextAudio() throws RemoteException, PlayerException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveLeft() throws RemoteException, PlayerException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveRight() throws RemoteException, PlayerException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addPlayerMessageListener(IPlayerListener listener)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removePlayerMessageListener(IPlayerListener listener)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void playPlayList(String pls) throws RemoteException,
+			PlayerException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
