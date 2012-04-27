7a55f2f62d1092068e9581567bdc973abff17f6b
sebastian <pichlmeier.sebastian@googlemail.com>
2012-04-27 10:05:43 +0200

    add directory send + receive test

--- de.remote.test/test/de/remote/test/RMITransceiverTest.java
+++ de.remote.test/test/de/remote/test/RMITransceiverTest.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import org.junit.Assert;
 import org.junit.Test;
 
+import de.newsystem.rmi.transceiver.DirectoryReceiver;
+import de.newsystem.rmi.transceiver.DirectorySender;
 import de.newsystem.rmi.transceiver.FileReceiver;
 import de.newsystem.rmi.transceiver.FileSender;
 import de.newsystem.rmi.transceiver.ReceiverProgress;
@@ -15,24 +17,37 @@ import junit.framework.TestCase;
 public class RMITransceiverTest extends TestCase {
 
 	/**
-	 * port for transceiver
+	 * port for file transceiver
 	 */
-	public static final int TRANSCEIVER_PORT = RMIChatTest.RMI_TEST_PORT_CLIENT + 3;
+	public static final int TRANSCEIVER_FILE_PORT = RMIChatTest.RMI_TEST_PORT_CLIENT + 3;
 
+	/**
+	 * port for directory transceiver
+	 */
+	public static final int TRANSCEIVER_DIRECTORY_PORT = RMIChatTest.RMI_TEST_PORT_CLIENT + 3;
+
+	/**
+	 * prefix for copied file
+	 */
 	public static final String TEST_FILE = "received_";
 
 	/**
-	 * perform transceiver test
+	 * suffix for copied directory
+	 */
+	public static final String TEST_COPY_DIRECTORY = "_copy";
+
+	/**
+	 * perform file transceiver test
 	 */
 	@Test
-	public void testTransceiver() {
+	public void testFileTransceiver() {
 		File src = new File(BrowserTest.TEST_LOCATION + MPlayerTest.TEST_FILE);
 		File dest = new File(BrowserTest.TEST_LOCATION + TEST_FILE
 				+ MPlayerTest.TEST_FILE);
 		try {
-			FileSender sender = new FileSender(src, TRANSCEIVER_PORT, 1);
+			FileSender sender = new FileSender(src, TRANSCEIVER_FILE_PORT, 1);
 			FileReceiver receiver = new FileReceiver("localhost",
-					TRANSCEIVER_PORT,10000, dest);
+					TRANSCEIVER_FILE_PORT, 50000, dest);
 			receiver.getProgressListener().add(new MyListener());
 			sender.sendAsync();
 			Thread.sleep(500);
@@ -45,9 +60,51 @@ public class RMITransceiverTest extends TestCase {
 		}
 		if (!dest.exists())
 			Assert.assertTrue("destiny file does not exist", false);
-		Assert.assertTrue("file size does not equal", src.length() == dest.length());
+		Assert.assertTrue("file size does not equal",
+				src.length() == dest.length());
 		if (dest.exists())
 			dest.delete();
+	}
+
+	/**
+	 * perform directory transceiver test
+	 */
+	@Test
+	public void testDirectoryTransceiver() {
+		File dest = new File(BrowserTest.TEST_LOCATION.substring(0,
+				BrowserTest.TEST_LOCATION.length() - 1) + TEST_COPY_DIRECTORY);
+		try {
+
+			DirectorySender sender = new DirectorySender(new File(
+					BrowserTest.TEST_LOCATION), TRANSCEIVER_DIRECTORY_PORT, 1);
+			DirectoryReceiver receiver = new DirectoryReceiver("localhost",
+					TRANSCEIVER_DIRECTORY_PORT, dest);
+			receiver.getProgressListener().add(new MyListener());
+			sender.sendAsync();
+			Thread.sleep(500);
+			receiver.receiveAsync();
+			Thread.sleep(500);
+		} catch (IOException e) {
+			Assert.assertTrue(e.getMessage(), false);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		if (!dest.exists())
+			Assert.assertTrue("destiny directory does not exist", false);
+		Assert.assertTrue("destiny contains no file", dest.list().length > 0);
+		delete(dest);
+	}
+
+	private void delete(File dest) {
+		if (dest.exists()) {
+			if (dest.isFile())
+				dest.delete();
+			else {
+				for (File str : dest.listFiles())
+					delete(str);
+				dest.delete();
+			}
+		}
 	}
 
 	public class MyListener implements ReceiverProgress {
