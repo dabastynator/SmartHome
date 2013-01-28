f4faa3950d2914927e0caeefdb066faa1d380069
sebastian <pichlmeier.sebastian@googlemail.com>
2013-01-28 20:29:00 +0100

    extend api for mouse and key board interaction

--- de.remote.api/src/de/remote/api/IControl.java
+++ de.remote.api/src/de/remote/api/IControl.java
@@ -8,24 +8,52 @@ import de.newsystem.rmi.protokol.RemoteException;
  * 
  * @author sebastian
  */
-public interface IControl  extends RemoteAble{
+public interface IControl extends RemoteAble {
 
 	/**
 	 * switch the display off
+	 * 
 	 * @throws RemoteException
 	 */
 	public void displayDark() throws RemoteException;
 
 	/**
 	 * switch the display on
+	 * 
 	 * @throws RemoteException
 	 */
 	public void displayBride() throws RemoteException;
 
 	/**
 	 * shutdown the computer
+	 * 
 	 * @throws RemoteException
 	 */
 	public void shutdown() throws RemoteException;
+
+	/**
+	 * Move the mouse cursor to specified position.
+	 * 
+	 * @param x
+	 * @param y
+	 * @throws RemoteException
+	 */
+	public void mouseMove(int x, int y) throws RemoteException;
+
+	/**
+	 * Performe specified mouse action.
+	 * 
+	 * @param button
+	 * @throws RemoteException
+	 */
+	public void mousePress(int button) throws RemoteException;
+
+	/**
+	 * Press specified key.
+	 * 
+	 * @param keycode
+	 * @throws RemoteException
+	 */
+	public void keyPress(int keycode) throws RemoteException;
 
 }
