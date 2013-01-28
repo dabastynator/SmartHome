8022ec3fb2a80a4ed63e23b168c893d22fb2d5bc
sebastian <pichlmeier.sebastian@googlemail.com>
2013-01-28 20:35:17 +0100

    Implement remote mouse and key board functionality using java robot api

--- de.remote.impl/src/de/remote/impl/ControlImpl.java
+++ de.remote.impl/src/de/remote/impl/ControlImpl.java
@@ -1,7 +1,11 @@
 package de.remote.impl;
 
+import java.awt.AWTException;
+import java.awt.Robot;
 import java.io.IOException;
 
+import de.newsystem.rmi.protokol.RemoteException;
+import de.remote.api.ControlConstants;
 import de.remote.api.IControl;
 
 public class ControlImpl implements IControl {
@@ -10,6 +14,8 @@ public class ControlImpl implements IControl {
 	public static final String MAKE_BRIDE = "xset dpms force on";
 	public static final String EXIT_SCREENSAVER = "gnome-screensaver-command --exit";
 	public static final String SHUTDOWN = "shutdown -h now";
+
+	private Robot robot;
 
 	@Override
 	public void shutdown() {
@@ -26,7 +32,7 @@ public class ControlImpl implements IControl {
 			Runtime.getRuntime().exec(MAKE_DARK);
 		} catch (IOException e) {
 			e.printStackTrace();
-		}		
+		}
 	}
 
 	@Override
@@ -36,7 +42,33 @@ public class ControlImpl implements IControl {
 			Runtime.getRuntime().exec(EXIT_SCREENSAVER);
 		} catch (IOException e) {
 			e.printStackTrace();
-		}		
+		}
+	}
+
+	private Robot getRobot() throws RemoteException {
+		if (robot == null)
+			try {
+				robot = new Robot();
+			} catch (AWTException e) {
+				throw new RemoteException(ControlConstants.STATION_ID,
+						"robot not available: " + e.getMessage());
+			}
+		return robot;
+	}
+
+	@Override
+	public void mouseMove(int x, int y) throws RemoteException {
+		getRobot().mouseMove(x, y);
+	}
+
+	@Override
+	public void mousePress(int button) throws RemoteException {
+		getRobot().mousePress(button);
+	}
+
+	@Override
+	public void keyPress(int keycode) throws RemoteException {
+		getRobot().keyPress(keycode);
 	}
 
 }
