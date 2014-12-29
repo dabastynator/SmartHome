7be97f2b0731b25ae774efb2e14afc82d06930c6
Sebastian <sebastian@asus-basti>
2014-12-29 22:41:03 +0100

    Convert family code correctly

--- de.neo.remote/src/de/neo/remote/gpio/SwitchPower.java
+++ de.neo.remote/src/de/neo/remote/gpio/SwitchPower.java
@@ -4,9 +4,7 @@ import java.util.BitSet;
 
 import com.pi4j.io.gpio.RaspiPin;
 
-import de.neo.remote.RemoteLogger;
 import de.neo.remote.api.IInternetSwitch.State;
-import de.neo.rmi.api.RMILogger.LogPriority;
 
 /**
  * Implements the gpio power by executing switch commands
@@ -24,17 +22,11 @@ public class SwitchPower {
 
 	public synchronized void setSwitchState(String familyCode,
 			int switchNumber, State state) {
-		BitSet bitSet = new BitSet(5);
-		for (int i = 0; i < 5; i++)
-			if (familyCode.charAt(i) == 1)
-				bitSet.set(i);
+		BitSet bitSet = RCSwitch.getSwitchGroupAddress(familyCode);
 		if (state == State.ON)
 			mRcSwitch.switchOn(bitSet, switchNumber);
 		else
 			mRcSwitch.switchOff(bitSet, switchNumber);
-		RemoteLogger.performLog(LogPriority.INFORMATION, "Set switch "
-				+ familyCode + " " + switchNumber + " to " + state,
-				"Internetswitch");
 	}
 
 }
