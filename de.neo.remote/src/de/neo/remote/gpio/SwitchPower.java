d91a345a10481570af93be4ce0a2035f12599ac9
Sebastian <sebastian@asus-basti>
2014-12-29 22:26:56 +0100

    Add log information for switch

--- de.neo.remote/src/de/neo/remote/gpio/SwitchPower.java
+++ de.neo.remote/src/de/neo/remote/gpio/SwitchPower.java
@@ -4,7 +4,9 @@ import java.util.BitSet;
 
 import com.pi4j.io.gpio.RaspiPin;
 
+import de.neo.remote.RemoteLogger;
 import de.neo.remote.api.IInternetSwitch.State;
+import de.neo.rmi.api.RMILogger.LogPriority;
 
 /**
  * Implements the gpio power by executing switch commands
@@ -30,6 +32,9 @@ public class SwitchPower {
 			mRcSwitch.switchOn(bitSet, switchNumber);
 		else
 			mRcSwitch.switchOff(bitSet, switchNumber);
+		RemoteLogger.performLog(LogPriority.INFORMATION, "Set switch "
+				+ familyCode + " " + switchNumber + " to " + state,
+				"Internetswitch");
 	}
 
 }
