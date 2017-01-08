1ac54c177bf2425f45437316aa41286fa1702e7f
Sebastian <pichlmeier.sebastian@gmail.com>
2017-01-08 19:41:11 +0100

    Update widges on screen-on with broadcast receiver

--- de.neo.remote.mobile/src/de/neo/remote/mobile/tasks/WidgetUpdateTask.java
+++ de.neo.remote.mobile/src/de/neo/remote/mobile/tasks/WidgetUpdateTask.java
@@ -1,7 +1,9 @@
 package de.neo.remote.mobile.tasks;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.PowerManager;
 import de.neo.remote.mobile.services.RemoteService;
 
@@ -12,13 +14,29 @@ public class WidgetUpdateTask extends Thread {
 	private boolean mRunning;
 	private Context mContext;
 
+	private ScreenReceiver mReceiver;
+
 	public WidgetUpdateTask(Context context) {
 		mContext = context;
 		mRunning = true;
+
+		IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+		filter.addAction(Intent.ACTION_SCREEN_OFF);
+		mReceiver = new ScreenReceiver();
+		mContext.registerReceiver(mReceiver, filter);
 	}
 
 	public void setRunning(boolean running) {
 		mRunning = running;
+		if (!running) {
+			mContext.unregisterReceiver(mReceiver);
+		}
+	}
+
+	private void sendUpdateAction() {
+		Intent serviceIntent = new Intent(mContext, RemoteService.class);
+		serviceIntent.setAction(RemoteService.ACTION_UPDATE);
+		mContext.startService(serviceIntent);
 	}
 
 	@Override
@@ -26,9 +44,9 @@ public class WidgetUpdateTask extends Thread {
 		while (mRunning) {
 			try {
 				sleep(UPDATE_PERIOD);
-				Intent serviceIntent = new Intent(mContext, RemoteService.class);
-				serviceIntent.setAction(RemoteService.ACTION_UPDATE);
-				mContext.startService(serviceIntent);
+				if (screenIsOn()) {
+					sendUpdateAction();
+				}
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -42,6 +60,17 @@ public class WidgetUpdateTask extends Thread {
 			return true;
 		}
 		return false;
+	}
+
+	class ScreenReceiver extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+				sendUpdateAction();
+			}
+		}
+
 	}
 
 }
